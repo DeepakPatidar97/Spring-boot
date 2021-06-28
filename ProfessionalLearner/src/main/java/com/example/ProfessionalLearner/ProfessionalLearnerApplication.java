1afc01fa463eef0818cac87087aa164aafd10821
Dell <Dell@DESKTOP-1UPL14R>
2021-06-28 09:23:45 +0530

    First Web Program.

---
+++ ProfessionalLearner/src/main/java/com/example/ProfessionalLearner/ProfessionalLearnerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProfessionalLearner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessionalLearnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessionalLearnerApplication.class, args);
+	}
+
+}
